f505d318061885fa114a0e51ef0a4a5826762427
ardi <rahmansuwarto@gmail.com>
2025-04-20 22:15:22 +0700

    header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Fily Premium Bootstrap Admin Dashboard Template</title>
+  <title>IBUKOTA NUSANTARA ROLEPLAY</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{url('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
   <link rel="stylesheet" href="{{url('assets/vendors/css/vendor.bundle.base.css')}}">
